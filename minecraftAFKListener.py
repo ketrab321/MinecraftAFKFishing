3a1d11070fd59918f84b177c6531eb377e41e771
ketrab321 <ketrab54321@gmail.com>
2022-12-28 17:33:15 +0100

    catching screen, creating text template, parsing rules json and handling actions

---
+++ minecraftAFKListener.py
@@ -0,0 +1,159 @@
+import pyautogui
+import time
+import win32api
+import win32con
+import cv2
+import json
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont, ImageGrab
+
+actionTypes = ["keyboard_press", "keyboard_key_down", "keyboard_key_up", "mouse_click", "mouse_button_down", "mouse_button_up", "mouse_move_horizontal", "mouse_move_vertical", "scroll", "wait"]
+
+def scroll(clicks=0, delta_x=0, delta_y=0, delay_between_ticks=0):
+    """
+    Source: https://learn.microsoft.com/en-gb/windows/win32/api/winuser/nf-winuser-mouse_event?redirectedfrom=MSDN
+
+    void mouse_event(
+      DWORD     dwFlags,
+      DWORD     dx,
+      DWORD     dy,
+      DWORD     dwData,
+      ULONG_PTR dwExtraInfo
+    );
+
+    If dwFlags contains MOUSEEVENTF_WHEEL,
+    then dwData specifies the amount of wheel movement.
+    A positive value indicates that the wheel was rotated forward, away from the user;
+    A negative value indicates that the wheel was rotated backward, toward the user.
+    One wheel click is defined as WHEEL_DELTA, which is 120.
+
+    :param delay_between_ticks: 
+    :param delta_y: 
+    :param delta_x:
+    :param clicks:
+    :return:
+    """
+
+    if clicks > 0:
+        increment = win32con.WHEEL_DELTA
+    else:
+        increment = win32con.WHEEL_DELTA * -1
+
+    for _ in range(abs(clicks)):
+        win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL, delta_x, delta_y, increment, 0)
+        time.sleep(delay_between_ticks)
+
+def generateRules(selectedRuleSet):
+    # Opening JSON file
+    filePath = "rules/" + selectedRuleSet
+
+    with open(filePath, encoding='utf-8') as f:
+        rules = []
+        tempRules = json.load(f)
+        
+        font = ImageFont.truetype("font/minecraft_font.ttf", size=32)
+        testImg = Image.new('RGB', (512, 512), color='blue')
+        testImgDraw = ImageDraw.Draw(testImg)
+        padding = 0
+
+        for rule in tempRules['rules']:
+            message = rule['textToLookFor']
+
+            message = message.replace(" ", "  ")
+            textWidth, textHeight = testImgDraw.textsize(message, font=font)
+
+            img = Image.new('RGB', (textWidth + 2 * padding, textHeight + 2 * padding), color='black')
+            imgDraw = ImageDraw.Draw(img)
+
+            imgDraw.text((padding, padding), message, font=font, fill=(255, 255, 255))
+
+            cv2_img = np.array(img)
+            cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_RGB2BGR)
+            cv2_img = process_image(cv2_img)
+
+            cv2.imwrite("temp/" + message + ".png", cv2_img)
+            rules.append(dict(
+                textToLookFor=message,
+                textImage=cv2_img,
+                actions=rule['actions']
+            ))
+    return rules
+        
+def process_image(img):
+    _,thresh_img = cv2.threshold(img,220,255,cv2.THRESH_BINARY)
+    return thresh_img
+
+def doAction(action):
+    actionType = action["type"]
+    actionData = action["data"]
+
+    if actionType = actionTypes[0]:
+        # keyboard_press
+
+    else if actionType = actionTypes[1]:
+        # keyboard_key_down
+
+    else if actionType = actionTypes[2]:
+        # keyboard_key_up
+
+    else if actionType = actionTypes[3]:
+        # mouse_click
+
+    else if actionType = actionTypes[4]:
+        # mouse_button_down
+
+    else if actionType = actionTypes[5]:
+        # mouse_button_up
+
+    else if actionType = actionTypes[6]:
+        # mouse_move_horizontal
+
+    else if actionType = actionTypes[7]:
+        # mouse_move_vertical
+
+    else if actionType = actionTypes[8]:
+        # scroll
+
+    else if actionType = actionTypes[9]:
+        # wait
+        time.sleep(actionData)
+
+def run():
+    # CONFIG
+    width = 1920
+    height = 1080
+
+    seconds = 0
+
+    rules = generateRules("fishing.json")
+    while(True):
+        time.sleep(0.5)
+        seconds += 0.5
+
+        cap = ImageGrab.grab(bbox =(width / 2 + 400, height/2, width, height - 160))
+        
+        cv2_img = np.array(cap)
+        textCap = cv2.cvtColor(cv2_img, cv2.COLOR_RGB2BGR)
+
+        textCap = process_image(textCap)
+
+        for rule in rules:
+            template = rule['textImage']
+            res = cv2.matchTemplate(textCap, template, cv2.TM_SQDIFF_NORMED)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+            
+            threshold = .5
+
+            if min_val < threshold:
+                print(seconds, "s : ", rule["textToLookFor"], " found!")
+                cv2.imwrite("temp/temp" + str(seconds) + ".png", textCap)
+
+                for action in rule["actions"]:
+                    doAction(action)
+
+            
+            # if found:
+            #     
+
+
+run()
